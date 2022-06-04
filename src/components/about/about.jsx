f770143d4aae29d0dd7990c87758706aae5cdd3c
abhivakt sharma <abhivyakt18sharma@gmail.com>
2022-06-04 17:59:58 +0530

    about section completed

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import './about.css'
-
+import ME from '../../assets/me2.jpg'
+import {Si1001Tracklists} from 'react-icons/si'
+import {TiPointOfInterest} from 'react-icons/ti'
+import {FaMedal} from 'react-icons/fa'
 const about =  () => {
     return (
-    <section id="about">About</section>
+    <section id="about">
+        <h5>
+            Get To Know
+        </h5>
+        <h2>
+            About Me
+        </h2>
+        <div className="container about_container">
+            <div className="about_me">
+                <div className="about_me-image">
+                <img src={ME} alt = "about-pic"/>
+                </div>
+            </div>
+        <div className="about_content">
+            <div className="about_cards">
+                <article className="about_card">
+                    <Si1001Tracklists className="about_icon"/>
+                    <h5>Hobbies</h5>
+                    <small>Basketball, Travelling , Cooking</small>
+                </article>
+                <article className="about_card">
+                    <TiPointOfInterest className="about_icon"/>
+                    <h5>Hobbies</h5>
+                    <small>Basketball, Travelling , Cooking</small>
+                </article>
+                <article className="about_card">
+                    <FaMedal className="about_icon"/>
+                    <h5>Hobbies</h5>
+                    <small>Basketball, Travelling , Cooking</small>
+                </article>
+            </div>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex quos ullam, porro recusandae accusamus possimus sapiente quia temporibus quae nemo quod velit inventore. Mollitia temporibus ab esse veniam ipsam veritatis?
+            </p>
+            <a href="#contact" className="btn btn-primary">Let's Talk</a> 
+        </div>
+        </div>
+
+    </section>
     )
 }
 
--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import './contact.css'
 const contact = () => {
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      {/* <a href='#about' className='scroll_up'>Scroll up</a> */}
+      Contact</section>
   )
 }
 
